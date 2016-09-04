8113a4f12ae1e562a39dcf9dc94582f66e49fe67
Itshalffull <Matt@getcrystal.net>
2016-09-04 23:49:34 +0000

    Finished boilerplate for trustgraph.sol

--- contracts/ReputationValueStatefull.sol
+++ contracts/ReputationValueStatefull.sol
@@ -1,6 +1,9 @@
 //Contract that defines standard for statefull reputation that can be updated by giving numerical values to content or users.
 import "./ReputationValueStateless.sol";
 contract ReputationValueStatefull is ReputationValueStateless{
+    //Function that allows a user to add a new type of reputation to a contract.
+    function addReputationType(bytes32 _hash) returns (bool success) {}
+    
     //function that allows an individual user to rate a piece of content with a specific rep type in a specific community.
 	function rateContent(int rating, uint _content, bytes32 _rephash, uint _community) returns (bool success) {}
 	
--- contracts/TrustGraph.sol
+++ contracts/TrustGraph.sol
@@ -1,24 +1,34 @@
 //Holds information abuot the trust graph and trust values, which are computed by values.js
-contract TrustGraph is ReputationValueStateless{
+contract TrustGraph is ReputationValueStatefull{
 
     //Basic values for reputation standard.
-    function minimumValueOf() constant returns(int min){
+    function minimumValueOf(bytes32 _value) constant returns(int min){
         min = -1;
     }
-    function maximumValueOf constant returns (int max){
+    function maximumValueOf(bytes32 _value) constant returns (int max){
         max = 1;
     }
     
+    //
     //Holds start sets
     mapping (uint community => address[] startset) startsets;
     
     //Holds list of values (mapped sequentially to a number)
     mapping (uint valueID => bytes32 value) values;
     
-    //Allows an individual to rate another individual in a specific value.  The rating must be between -1 and 1
-    function addRating(uint _value, fixed _rating) returns (bool success){}
+    //Returns a list of all the values that an individual has been rated on (by iterating over the mapping)
+    function getReputationTypes() returns (bytes32[] _values) {}
     
-    //Gets the valuescore of an individual 
-    function reputationOf (uint _valueid, )
+    //Adds a new value to the system
+    function addReputationType(bytes32 _hash) returns (bool success) {}
+    
+    //Allows an individual to rate another individual in a specific value.  The rating must be between -1 and 1
+    function rateUser(int rating, address _ratee, bytes32 _valuename, uint _community) returns (bool success) {}
+   
+   //This is part of our standard but doesn't exist for this reputation type. We should split the reputation types in the future to content ratings and user ratings to avoid useless functions like this 
+    function rateContent(int rating, uint _content, bytes32 _rephash, uint _community) returns (bool success) {throw;}
+    
+    //Gets the valuescore of an individual, this is actually calculated offchain for the Proof of Concept and is simply data here
+    function reputationOf (address _agent, bytes32 _rephash, _uint _community) returns (uint range) {}
     
 }
